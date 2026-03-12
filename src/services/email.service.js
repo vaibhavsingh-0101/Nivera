1e6b88fdf7119b408caa4173a7022a4a628fd243
ARGroup12 <pragatiskillacademi@gmail.com>
2026-03-13 01:10:48 +0530

    for mail error

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -2,7 +2,11 @@ import nodemailer from "nodemailer"
 
 const transporter = nodemailer.createTransport({
   service: "gmail",
+   host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false, 
   auth: {
+
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS
   }
