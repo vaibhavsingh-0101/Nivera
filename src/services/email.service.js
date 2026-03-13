05743b65e7880c14383846a3cf6e67d933ef31ac
ARGroup12 <pragatiskillacademi@gmail.com>
2026-03-13 21:20:17 +0530

    for email services

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -5,6 +5,7 @@ const transporter = nodemailer.createTransport({
    host: process.env.SMTP_HOST,
   port: process.env.SMTP_PORT,
   secure: false, 
+    family: 4,
   auth: {
 
     user: process.env.SMTP_USER,
