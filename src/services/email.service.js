50985cff645950a12c854e14dca9e840e84e8e29
ARGroup12 <pragatiskillacademi@gmail.com>
2026-03-14 00:33:02 +0530

    server

--- src/services/email.service.js
+++ src/services/email.service.js
@@ -3,7 +3,7 @@ import dns from "dns"
 dns.setDefaultResultOrder("ipv4first")
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
+  host: "74.125.140.108", 
   port: 587,
   secure: false,
   family: 4, // force IPv4
